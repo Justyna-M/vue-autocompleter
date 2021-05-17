c75c8985f6987736ec536d64882895c3a12eb076
Justyna <mastek@student.agh.edu.pl>
2021-05-17 20:52:10 +0200

    dokumentacja do kodu

--- autocompleter.js
+++ autocompleter.js
@@ -9,22 +9,41 @@ Vue.component('v-autocompleter', {
     }
   },
   methods: {
+    /**
+     * Funkcja ustawiająca klikniętą propozycję jako wynik wyszukiwania i wystawiająca event, który powoduje zmianę strony na stronę z rezultatami wyszukiwania.
+     * @param {wybrana nazwa miasta} name 
+     */
     handleClick: function (name) {
       this.googleSearch = name;
       this.isActive = 1;
       this.$emit('enter', this.googleSearch)
     },
+    /**
+     * Funkcja formatująca propozycje wyszukiwania według wzory: wszukiwana fraza jest zapisana stylem normalnym, reszta -pogrubionym.
+     * @param {propozycje wyników wyszukiwania z daną frazą} wyraz 
+     * @returns sformatowany napis
+     */
     highlight: function (wyraz) {
       return wyraz.replaceAll(this.googleSearch, '<span class="highlight">' + this.googleSearch + '</span>')
     },
+    /**
+     * Funkcja pozwalająca zmienić stronę po wciśnięciu enter na stronę z wynikami wyszukiwnaia.
+     */
     zmienStrone: function () {
       this.$emit('enter', this.googleSearch)
     },
+    /**
+     * Funkcja wystawiająca event przy zmianie inputa.
+     */
     signalChange: function () {
       this.$emit('input')
     },
   },
   computed: {
+    /**
+     * Filtruje listę miast w poszukiwaniu nazw, zawierających wpisaną frazę.
+     * @returns zwraca 10 propozycji miast zawierających wpisaną frazę w postaci listy
+     */
     filteredCities: function () {
       if (this.googleSearch.length == 0) {
         return
